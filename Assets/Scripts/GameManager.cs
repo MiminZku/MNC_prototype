eb8fcf66e5ca27f893e32843982fe681d14d2456
MiminZku <82875083+MiminZku@users.noreply.github.com>
2023-01-24 16:43:23 +0900

    photon

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,10 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 // ================================
 // <GameManager class>
@@ -12,10 +15,21 @@ using UnityEngine.UI;
 // 
 // ================================
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks
 {
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance == null) instance = FindObjectOfType<GameManager>();
+
+            return instance;
+        }
+    }
+    private static GameManager instance;
+
     // <- ^ _ ->
-    [SerializeField] Text movePath;
+    //[SerializeField] Text movePath;
     [SerializeField] int boardRow = 6;
     [SerializeField] int boardCol = 3;
     [SerializeField] GameObject tiles;
@@ -23,22 +37,24 @@ public class GameManager : MonoBehaviour
 
     public Tile[,] board;
 
-    
+    public Transform[] spawnPositions;
+    public GameObject playerPrefab;
+    public bool isPlayersMoving;
+
 
     // Start is called before the first frame updates
     void Start()
     {
-        
-        // 부모 오브젝트 tiles를 통해서 tile을 가져와서 board에 넣어주기
-        board = new Tile[boardRow, boardCol];
-        for(int i = 0; i < boardRow; i++)
-        {
-            for(int j = 0; j < boardCol; j++)
-            {
-                Transform child = tiles.transform.GetChild(i*boardCol + j);
-                board[i, j] = child.GetComponent<Tile>();
-            }
-        }
+        //// 부모 오브젝트 tiles를 통해서 tile을 가져와서 board에 넣어주기
+        //board = new Tile[boardRow, boardCol];
+        //for(int i = 0; i < boardRow; i++)
+        //{
+        //    for(int j = 0; j < boardCol; j++)
+        //    {
+        //        Transform child = tiles.transform.GetChild(i*boardCol + j);
+        //        board[i, j] = child.GetComponent<Tile>();
+        //    }
+        //}
         // check
         // for(int i = 0; i < boardRow; i++)
         // {
@@ -48,11 +64,35 @@ public class GameManager : MonoBehaviour
         //         Debug.Log(board[i,j].transform.position);
         //     }
         // }
+        SpawnPlayer();
+    }
+    private void SpawnPlayer()
+    {
+        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+        var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition.position, spawnPosition.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PhotonNetwork.PlayerList.Length < 2) return;
+        foreach (Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        {
+            Hashtable cp = player.CustomProperties;
+            Debug.Log(cp["isMoveReady"]);
+            if (!(bool)cp["isMoveReady"]) return;
+        }
+        if (isPlayersMoving) return;
+        isPlayersMoving = true;
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            player.GetComponent<Player>().MoveBoth();
+        }
+    }
+
+    public override void OnLeftRoom() // 방 나가질 때 자동 실행(내가 나갈 경우에만)
+    {
+        SceneManager.LoadScene("Lobby");
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 // ================================
 // <Player class>
@@ -8,7 +10,7 @@ using UnityEngine;
 // -> 이동을 위한 입력값 받음
 // -> 실제 이동 구현
 // ================================
-public class Player : MonoBehaviour
+public class Player : MonoBehaviourPun
 {
     
     public Tile[,] board;
@@ -17,35 +19,126 @@ public class Player : MonoBehaviour
     // flags
     bool isPossibleMove = false;
 
+    private MeshRenderer meshRenderer;
+    Vector3 currentPosition;
+    public Material player1Mat;
+    public Material player2Mat;
+    [HideInInspector] public enum Move
+    {
+        None,
+        Up,
+        Left,
+        Down,
+        Right
+    }
+    public Move[] move = new Move[6];
+    public bool isMoveReady;
+    int moveCount = 0;
+    bool isMoving;
+
     void Start()
     {
-        
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { "isMoveReady", false } });
+        GetComponent<PhotonTransformView>().m_SynchronizePosition = false;
+        currentPosition = gameObject.transform.position;
+        meshRenderer = GetComponent<MeshRenderer>();
+        if (photonView.IsMine)
+        {
+            meshRenderer.material = player1Mat;
+        }
+        else
+        {
+            meshRenderer.material = player2Mat;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(isPossibleMove)
-        {   
-            float horizon = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            if(horizon > 0)
+        if (!photonView.IsMine) { return; }
+        if (isMoving) { return; }
+        if (moveCount == move.Length)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))    // 엔터키
             {
-                // 오른쪽으로 이동
-            }
-            else
-            {
-                // 왼쪽
-            }
-            if(vertical > 0)
-            {
-                // 위로 
-            }
-            else
-            {
-                // 아래로 
+                ReadyToMove();
             }
         }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                move[moveCount++] = Move.Up;
+                transform.Translate(new Vector3(0, 0, 1));
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                move[moveCount++] = Move.Left;
+                transform.Translate(new Vector3(-1, 0, 0));
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                move[moveCount++] = Move.Down;
+                transform.Translate(new Vector3(0, 0, -1));
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                move[moveCount++] = Move.Right;
+                transform.Translate(new Vector3(1, 0, 0));
+            }
+        }
+
+
+    }
+
+    public void ReadyToMove()
+    {
+        isMoveReady = true;
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { "isMoveReady", true } });
+        transform.position = currentPosition;
+    }
+
+    public void MoveBoth()
+    {
+        Debug.Log("MoveBoth");
+        isMoving = true;
+        GetComponent<PhotonTransformView>().m_SynchronizePosition = true;
+        StartCoroutine("MoveDelay");
+    }
+
+    IEnumerator MoveDelay()
+    {
+        if (!photonView.IsMine)
+        {
+            yield break;
+        }
+        isMoveReady = false;
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { "isMoveReady", false } });
+        for (int i = 0; i < move.Length; i++)
+        {
+            if (move[i] == Move.None) { continue; }
+            switch (move[i])
+            {
+                case Move.Up:
+                    transform.Translate(new Vector3(0, 0, 1));
+                    break;
+                case Move.Left:
+                    transform.Translate(new Vector3(-1, 0, 0));
+                    break;
+                case Move.Down:
+                    transform.Translate(new Vector3(0, 0, -1));
+                    break;
+                case Move.Right:
+                    transform.Translate(new Vector3(1, 0, 0));
+                    break;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+        moveCount = 0;
+        move = new Move[6];
+        currentPosition = transform.position;
+        isMoving = false;
+        GetComponent<PhotonTransformView>().m_SynchronizePosition = false;
+        GameManager.Instance.isPlayersMoving = false;
     }
 }
